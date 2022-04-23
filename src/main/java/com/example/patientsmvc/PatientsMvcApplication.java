0c8dabc6b98e9e0447f3d6de1d8c05783798a107
Sedyame Hamza <hamzasedyame61@gmail.com>
2022-04-23 11:34:04 +0000

    First commit

---
+++ src/main/java/com/example/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,30 @@
+package com.example.patientsmvc;
+
+import com.example.patientsmvc.entities.Patient;
+import com.example.patientsmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner CommandLineRunner(PatientRepository patientRepository){
+        return args -> {
+            patientRepository.save(new Patient(null,"Hassan",new Date(),false,12));
+            patientRepository.save(new Patient(null,"Hamza",new Date(),true,125));
+            patientRepository.save(new Patient(null,"Othmane",new Date(),true,182));
+            patientRepository.save(new Patient(null,"Marwane",new Date(),false,512));
+            patientRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+        };
+    }
+}
